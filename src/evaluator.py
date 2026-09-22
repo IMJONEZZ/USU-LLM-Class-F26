9aa87c1c135be501df3e4c2e2bad034e3f013c8e
cameronswapp <a02324736@usu.edu>
2026-09-22 11:51:28 -0600

    Built evaluator, no tests yet

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,1 +1,12 @@
-# Put evaluator here
+from datasets import load_dataset  # pragma: no cover
+from evaluate import evaluator
+
+data = load_dataset("UCLNLP/adversarial_qa", "adversarialQA", split="validation")
+
+qa_evaluator = evaluator("question-answering")
+
+results = qa_evaluator.compute(
+    model_or_pipeline="csarron/bert-base-uncased-squad-v1", data=data, metric="squad"
+)
+
+print(results)
